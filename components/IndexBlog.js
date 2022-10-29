10581aba90e6f639b96afa2d6f86581e5fc94dbc
Kay Patel <patelkh@oregonstate.edu>
2022-10-29 16:00:38 -0700

    Replaced getStaticProps with getServerSideProps

--- components/IndexBlog.js
+++ components/IndexBlog.js
@@ -1,21 +1,21 @@
 import React from "react";
-import styles from './../styles/BlogList.module.css';
+import styles from "./../styles/BlogList.module.css";
 import Link from "next/link";
 
-export default function IndexBlog({blog}) {
-    return (
-        <>
-        <Link href={`/blog/${blog._id}`}>
-            <div className={styles.blog} key={blog._id}>
-            <div>
-                <h1 className={styles.h1}>{blog.title}</h1>
-                <p className={styles.p}>{blog.author}</p>
-            </div>
-            <div>
-                <button className={styles.button}>View Post</button>
-            </div>
-            </div>
-            </Link>
-        </>
-    )
+export default function IndexBlog({ blog }) {
+  return (
+    <>
+      <Link href={`/blog/${blog._id}`}>
+        <div className={styles.blog} key={blog._id}>
+          <div>
+            <h2 className={styles.h1}>{blog.title}</h2>
+            <p className={styles.p}>{blog.author}</p>
+          </div>
+          <div>
+            <button className={styles.button}>View Post</button>
+          </div>
+        </div>
+      </Link>
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,26 @@ import Splash from "../components/Splash";
 import { GetStaticProps } from "next";
 import BlogList from "../components/BlogList";
 
-export const getStaticProps = async () => {
+// export const getStaticProps = async () => {
+//   const res = await fetch(
+//     "https://kays-travel-blog-api.herokuapp.com/blogs/view"
+//   );
+//   const blogs = await res.json();
+//   // console.log(blogs);
+//   return {
+//     props: {
+//       blogs,
+//     },
+//   };
+// };
+
+export const getServerSideProps = async () => {
   const res = await fetch(
     "https://kays-travel-blog-api.herokuapp.com/blogs/view"
   );
   const blogs = await res.json();
-  // console.log(blogs);
   return {
-    props: {
-      blogs,
-    },
+    props: { blogs },
   };
 };
 
@@ -20,8 +30,8 @@ export default function Blog({ blogs }) {
   // console.log(`Blogs: ${blogs}`)
   return (
     <>
-    <Splash />
-    <BlogList blogs={blogs} />    
+      <Splash />
+      <BlogList blogs={blogs} />
     </>
   );
 }
